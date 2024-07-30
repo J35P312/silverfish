99223732d84e4d5894d5aaf2a2155b39a3d3a4a9
Jesper Eisfeld <jeseis@K1315RW6KK432LK.local>
2024-07-30 12:32:28 +0200

    modified:   README.md 	modified:   silverfish.py 	new file:   test_data/1_1598414_1598580_0.bam 	new file:   test_data/1_1598414_1598580_0.bam.bai 	new file:   test_data/1_1598414_1598580_0.bam.fermi2.bam 	new file:   test_data/1_1598414_1598580_0.bam.fermi2.bam.bai 	new file:   test_data/1_1598414_1598580_0.bam.fermi2.bam.vcf 	new file:   test_data/1_1598414_1598580_0.fasta

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # silverfish
 Local de novo assembly project, De bruijn graph asssembler using the networkx module
+
+# Run
+
+python silverfish.py <input_fasta> > <output_fasta>
+
+Example:
+
+python silverfish.py test_data/1_1598414_1598580_0.fasta
+
+note the assmebler does not handle reads from different stands, I suggest reverse complementing reads on the negative strand before running.
+
+# Dependencies
+
+python3, networkx, matplotlib
--- silverfish.py
+++ silverfish.py
@@ -1,23 +1,20 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import time
+import sys
 
-k=5
-min_branch_length=2
-min_coverage=2
+def read_fasta(fasta_path):
+	reads={}
 
-min_overlap=0.2
-max_overlap=0.8
+	readname=0
+	for line in open(fasta_path):
+		if line[0] == ">":
+			readname+=1		
+		else:
+			reads[str(readname)]=line.strip()
 
+	return(reads)
 
-read="ATTGCCGATATTA"
-read3="ATTGCCGATATTA"
-read2="ATATTACCCCAAATTG"
-read5="ATTGCCGATCCCCAAATTG"
-
-t=time.time()
-
-reads={"1":read,"2":read2,"3":read3,"4":read2[0:10],"5":read5,"6":read5,"7":read5,"8":read,"9":read2}
 
 
 def find_chain(graph,start,ends):
@@ -33,7 +30,6 @@ def find_chain(graph,start,ends):
 		current_node=graph.successors(current_node)
 		current_node=list(current_node)
 
-		#print(current_node)
 		if not current_node:
 			break
 		current_node=current_node[0]
@@ -48,8 +44,6 @@ def find_chain(graph,start,ends):
 			return(chain)
 
 		successor_node=successor_node[0]
-
-
 	return(chain)
 
 
@@ -67,127 +61,185 @@ def kmerizer(seq,k):
 		
 	return([read_graph,kmers[0],kmers[-1]])
 
-kmers={}
-for read in reads:
-	print(reads[read])
-	kmers[read]=kmerizer(reads[read],k)
+def graph_to_seq(graph):
+	seq=[]
+
+	nodes=graph.nodes()
+	starting_point=False
+
+	for node in nodes:
+		successors=list(graph.successors(node))
+		predecessors=list(graph.predecessors(node))
+
+		if not predecessors and starting_point:
+			print("error, multiple starting points")
+			quit()
+
+		if not predecessors:
+			starting_point=node
+
+	node=starting_point
+	seq=[node]
+	while True:
+		node=list(graph.successors(node))
+		if not node:
+			break
+		seq.append(node[0][-1])
+		node=node[0]
+
+	return("".join(seq))
+
+def add_edge_weight(reads,graph):
+	for edge in graph.edges:
+			graph[edge[0]][edge[1]]["weight"]=0
+
+	for read in reads:
+		for edge in reads[read][0].edges:
+			graph[edge[0]][edge[1]]["weight"]+=1
+
+
+
+	return(graph)
+
+def trim_edges(graph,min_weight):
+	edge_list=list(graph.edges)
+	for edge in edge_list:
+		if graph[edge[0]][edge[1]]["weight"] < min_weight:
+			graph.remove_edge(edge[0],edge[1])
+	return(graph)
+
+
+def main(reads,k,min_support):
+
+	kmers={}
+	for read in reads:
+		kmers[read]=kmerizer(reads[read],k)
 	
-graph = nx.DiGraph()
-#add all nodes
-for read in kmers:
-	graph.update(kmers[read][0])
-nodes=set(graph.nodes)
-
-starting_point=set([])
-end_point=set([])
-branch_start=set([])
-branch_end=set([])
-
-for node in nodes:
-	successors=list(graph.successors(node))
-	predecessors=list(graph.predecessors(node))
-
-	if not successors:
-		end_point.add(node)
-	if not predecessors:
-		starting_point.add(node)
-
-	if len(successors) > 1:
-		branch_start.add(node)
-
-	if len(predecessors) > 1:
-		branch_end.add(node)
-
-#print(starting_point)
-#print(end_point)
-#print(branch_start)
-#print(branch_end)
+	graph = nx.DiGraph()
+	#add all nodes
+	for read in kmers:
+		graph.update(kmers[read][0])
+	nodes=set(graph.nodes)
 
 
-stretches=[]
-print(starting_point.union(branch_start))
+	graph=add_edge_weight(kmers,graph)
+	graph=trim_edges(graph,min_support)
 
-branch_start_chains={}
-branch_end_chains={}
+	starting_point=set([])
+	end_point=set([])
+	branch_start=set([])
+	branch_end=set([])
 
-for start in starting_point.union(branch_start,branch_end):	
-	for node in graph.successors(start):
-		node=str(node)
-		chain=[start]+find_chain(graph,node,branch_end.union(branch_start))
-		if len(chain) > min_branch_length:
-			stretches.append(chain)
+	for node in nodes:
+		successors=list(graph.successors(node))
+		predecessors=list(graph.predecessors(node))
 
-for i in range(0,len(stretches)):
-	if stretches[i][0] in branch_end:
-		branch_end_chains[stretches[i][0]]=i
-	if stretches[i][-1] in branch_start:
-		branch_start_chains[stretches[i][-1]]=i
+		if not successors:
+			end_point.add(node)
+		if not predecessors:
+			starting_point.add(node)
+
+		if len(successors) > 1:
+			branch_start.add(node)
+
+		if len(predecessors) > 1:
+			branch_end.add(node)
+
+	stretches=[]
+	branch_start_chains={}
+	branch_end_chains={}
+
+	for start in starting_point.union(branch_start,branch_end):	
+		for node in graph.successors(start):
+			node=str(node)
+			chain=[start]+find_chain(graph,node,branch_end.union(branch_start))
+			if len(chain) > min_branch_length:
+				stretches.append(chain)
+
+	for i in range(0,len(stretches)):
+		if stretches[i][0] in branch_end:
+			branch_end_chains[stretches[i][0]]=i
+		if stretches[i][-1] in branch_start:
+			branch_start_chains[stretches[i][-1]]=i
 
 
-
-#stretch_graph= nx.DiGraph()
-stretch_graph=[]
-bubble_arcs=[]
+	stretch_graph=[]
+	bubble_arcs=[]
 
 	
-bridging_reads={}
-scaffold_graph=nx.DiGraph()
-for i in range(0,len(stretches)):
-	#stretch_graph.add_node( i )
-	if stretches[i][0] in branch_start and stretches[i][-1] in branch_end:
-		bubble_arcs.append(i)
-		#print(i)
-		#print(stretches[i])
+	bridging_reads={}
+	scaffold_graph=nx.DiGraph()
+	for i in range(0,len(stretches)):
+		#stretch_graph.add_node( i )
+		if stretches[i][0] in branch_start and stretches[i][-1] in branch_end:
+			bubble_arcs.append(i)
 
-	bridging_reads[i]=set([])
-	stretch_graph.append( nx.subgraph(graph,stretches[i]) )
+		bridging_reads[i]=set([])
+		stretch_graph.append( nx.subgraph(graph,stretches[i]) )
 
+	scaffolds={}
+	scaffolded_arcs=set([])
 
+	for i in range(0,len(stretches)):
+		chain=stretches[i]
 
-print(branch_start_chains)
-print(branch_end_chains)
+		scaffold=nx.DiGraph(stretch_graph[i])
+		scaffolded=set([])
 
-scaffolds={}
-scaffolded_arcs=set([])
+		if chain[0] in starting_point:
+			continue
 
-for i in range(0,len(stretches)):
-	chain=stretches[i]
+		if chain[0] in branch_start and chain[0] in branch_start_chains:		
+			scaffold.update(stretch_graph[branch_start_chains[chain[0]]])
+			scaffolded_arcs.add(branch_start_chains[chain[0]])
+		if chain[-1] in branch_end and chain[-1] in branch_end_chains:		
+			scaffold.update(stretch_graph[branch_end_chains[chain[-1]]])
+			scaffolded_arcs.add(branch_end_chains[chain[-1]])
 
-	scaffold=nx.DiGraph(stretch_graph[i])
-	scaffolded=set([])
-
-	if chain[0] in starting_point:
-		continue
-
-	if chain[0] in branch_start:		
-		scaffold.update(stretch_graph[branch_start_chains[chain[0]]])
-		scaffolded_arcs.add(branch_start_chains[chain[0]])
-	if chain[-1] in branch_end:		
-		scaffold.update(stretch_graph[branch_end_chains[chain[-1]]])
-		scaffolded_arcs.add(branch_end_chains[chain[-1]])
-
-	if scaffold:	
-		scaffolds[i]=scaffold
+		if scaffold:	
+			scaffolds[i]=scaffold
 		
-final_scaffolds=[]	
-for i in range(0,len(stretches)):
-	if i in scaffolds:
-		final_scaffolds.append(scaffolds[i])
-	elif i in scaffolded_arcs:
-		continue
-	else:
-		final_scaffolds.append(stretch_graph[i])
+	final_scaffolds=[]	
+	for i in range(0,len(stretches)):
+		if i in scaffolds:
+			final_scaffolds.append(scaffolds[i])
+		elif i in scaffolded_arcs:
+			continue
+		else:
+			final_scaffolds.append(stretch_graph[i])
+
+	scaffold_id=1
+	for scaffold in final_scaffolds:
+		print(f">scaffold_{scaffold_id}")
+		print(graph_to_seq(scaffold))
+		scaffold_id+=1
+
+	#subax1 = plt.subplot(131)
+	nx.draw(final_scaffolds[0], with_labels=True, font_weight='bold')
+	#subax2 = plt.subplot(132)
+	#nx.draw(final_scaffolds[1], with_labels=True, font_weight='bold')
+	#subax4 = plt.subplot(133)
+	#nx.draw(graph, with_labels=True, font_weight='bold')
+
+	#plt.show()
 
 
+k=101
+min_branch_length=2
+min_support=3
 
-print(time.time()-t)
+min_overlap=0.2
+max_overlap=0.8
 
-#subax1 = plt.subplot(131)
-#nx.draw(final_scaffolds[0], with_labels=True, font_weight='bold')
-#subax2 = plt.subplot(132)
-#nx.draw(final_scaffolds[1], with_labels=True, font_weight='bold')
-#subax4 = plt.subplot(133)
-#nx.draw(graph, with_labels=True, font_weight='bold')
 
-#plt.show()
+read="ATTGCCGATATTA"
+read3="ATTGCCGATATTA"
+read2="ATATTACCCCAAATTG"
+read5="ATTGCCGATCCCCAAATTG"
+
+#t=time.time()
+#reads={"1":read,"2":read2,"3":read3,"4":read2[0:10],"5":read5,"6":read5,"7":read5,"8":read,"9":read2}
+
+reads=read_fasta(sys.argv[1])
+main(reads,k,min_support)
 
